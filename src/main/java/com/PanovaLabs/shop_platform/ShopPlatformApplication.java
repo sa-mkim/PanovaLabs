421de77be94bca45331e6f5af27b175d8a8e5534
조재령 <l_arti0302@kakao.com>
2025-03-07 16:04:20 -0600

    init

---
+++ src/main/java/com/PanovaLabs/shop_platform/ShopPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.PanovaLabs.shop_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PanovaLabs/shop_platform/ShopPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PanovaLabs.shop_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
